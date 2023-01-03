27a1b1a8eace69c24ee959f57297290a398ebcf7
Ruslan-Synytsia <mymessage02@gmail.com>
2023-01-03 22:24:47 +0200

    Web app

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,15 +1,9 @@
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React from "react";
 import style from './App.module.css';
-import Header from "../Header/Header";
 
 const App = () => {
-    const dispatch = useDispatch();
-    const initApp = useSelector(state => state.initApp.isInitApp);
-
     return (
         <div className={style.App}>
-            <Header />
         </div>
     )
 }
