f4b2fe9e624f830eb85c2f16e4883e497028a6b7
Ruslan-Synytsia <mymessage02@gmail.com>
2023-01-05 03:04:36 +0200

    '05.01.23'

--- src/API/api.js
+++ src/API/api.js
@@ -7,7 +7,7 @@ const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     withCredentials: true,
     params: {
-        api_key: '0471cac914f115a568e4ebde8feb5fd4'
+        api_key: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDcxY2FjOTE0ZjExNWE1NjhlNGViZGU4ZmViNWZkNCIsInN1YiI6IjYzYjQ2ZDc3ODc1ZDFhMDA4MjAzNWRlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ce9Q79StaGqXsgY5ZOS_jM_ON-s1-3yy2eiktd5IPAk'
     }
     // ,
     // headers: {
