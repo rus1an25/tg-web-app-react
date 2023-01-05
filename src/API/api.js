9701d2b14a9b3cd8039b50391c2ff95aa89eb7f9
Ruslan-Synytsia <mymessage02@gmail.com>
2023-01-05 03:06:22 +0200

    '05.01.23'

--- src/API/api.js
+++ src/API/api.js
@@ -8,14 +8,13 @@ const instance = axios.create({
     withCredentials: true,
     params: {
         api_key: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDcxY2FjOTE0ZjExNWE1NjhlNGViZGU4ZmViNWZkNCIsInN1YiI6IjYzYjQ2ZDc3ODc1ZDFhMDA4MjAzNWRlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ce9Q79StaGqXsgY5ZOS_jM_ON-s1-3yy2eiktd5IPAk'
+    },
+    headers: {
+        'Access-Control-Allow-Origin': 'https://api.themoviedb.org/3',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
     }
-    // ,
-    // headers: {
-    //     'Access-Control-Allow-Origin': 'https://api.themoviedb.org/3',
-    //     'Access-Control-Allow-Credentials': true,
-    //     'Access-Control-Allow-Headers': '*',
-    //     'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
-    // }
 });
 
 export const requestToAPI = {
