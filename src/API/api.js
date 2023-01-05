fc7fee72a4e0c8fdfc0ac4386a31ddded2ad104e
Ruslan-Synytsia <mymessage02@gmail.com>
2023-01-05 03:34:35 +0200

    '05.01.23'

--- src/API/api.js
+++ src/API/api.js
@@ -5,15 +5,15 @@ let lang = 'en-EN';
 
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
-    // withCredentials: true,
-    params: {
-        api_key: '0471cac914f115a568e4ebde8feb5fd4'
-    },
     headers: {
-        'Access-Control-Allow-Origin': 'https://api.themoviedb.org/3',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDcxY2FjOTE0ZjExNWE1NjhlNGViZGU4ZmViNWZkNCIsInN1YiI6IjYzYjQ2ZDc3ODc1ZDFhMDA4MjAzNWRlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ce9Q79StaGqXsgY5ZOS_jM_ON-s1-3yy2eiktd5IPAk"
+    },
+    data: {
+        "authKey": API_KEY,
+        "username": "rus1an251",
+        "passkey": "rus1984lan"
     }
 });
 
